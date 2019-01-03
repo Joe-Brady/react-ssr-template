4e08b730cdc9e0d4578b251554461fb3678b55d0
Joe Brady <joe.brady@uk.ibm.com>
2019-01-03 23:29:13 +0000

    WORKING!

--- client/index.js
+++ client/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Loadable from "react-loadable";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+Loadable.preloadReady().then(() => {
+  ReactDOM.hydrate(<App />, document.getElementById("root"));
+});
--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const webpack = require("webpack");
 const config = require("../webpack.client.js");
 const webpackDevMiddleware = require("webpack-dev-middleware");
 const webpackHotMiddleware = require("webpack-hot-middleware");
+import React from "react";
 import ReactDOMServer from "react-dom/server";
+import App from "../client/components/App";
+import Loadable from "react-loadable";
+import { getBundles } from "react-loadable/webpack";
+import stats from "../dist/react-loadable.json";
 
 const app = express();
 const compiler = webpack(config);
@@ -12,22 +17,37 @@ app.use(webpackDevMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
 
 app.get("*", (req, res) => {
+  let modules = [];
+
+  let html = ReactDOMServer.renderToString(
+    <Loadable.Capture report={moduleName => modules.push(moduleName)}>
+      <App />
+    </Loadable.Capture>
+  );
+
+  let bundles = getBundles(stats, modules);
+
   res.send(
     `<html>
       <head>
         <title>React Hot Loader Minimal Boilerplate</title>
       </head>
       <body>
-        <div id="root"></div>
+        <div id="root">${html}</div>
+        <script src="/dist/manifest.js"></script>
+        ${bundles
+          .map(bundle => {
+            return `<script src="/dist/${bundle.file}"></script>`;
+          })
+          .join("\n")}
         <script src="app.js"></script>
       </body>
     </html>`
   );
 });
 
-app.listen(3000, err => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("Listening on http://localhost:3000");
+Loadable.preloadAll().then(() => {
+  app.listen(3000, () => {
+    console.log("Running on http://localhost:3000/");
+  });
 });
