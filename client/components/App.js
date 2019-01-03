f35700d7aa1f00322be9811d81c76e3cb0eec0e9
Joe Brady <joe.brady@uk.ibm.com>
2019-01-03 15:33:05 +0000

    Add loadable

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import Loadable from "react-loadable";
 
-const App = () => <div>Hellooo World!</div>;
+const MyComponentLoadable = Loadable({
+  loader: () => import("./MyComponent"),
+  loading: () => <div>Loading...</div>
+});
+
+const App = () => (
+  <div>
+    <p>This is App.js</p>
+    <MyComponentLoadable />
+  </div>
+);
 
 export default hot(App);
---
+++ client/components/MyComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MyComponent = () => (
+  <div>
+    <h1>My component here</h1>
+  </div>
+);
+
+export default MyComponent;
