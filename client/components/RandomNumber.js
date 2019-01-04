fc37fd6c91374098d10fb5c8fe7c33a949a22cd9
Joe Brady <joe.brady@uk.ibm.com>
2019-01-04 11:14:59 +0000

    Add note

--- client/components/RandomNumber.js
+++ client/components/RandomNumber.js
@@ -3,6 +3,10 @@ import React from "react";
 const RandomNumber = () => (
   <div>
     <h1>Random number component:</h1>
+    <p>
+      Note that the random number is generated twice - once on the server render
+      and again when the client 'hydrates' the window:
+    </p>
     <p>{Math.random()}</p>
   </div>
 );
