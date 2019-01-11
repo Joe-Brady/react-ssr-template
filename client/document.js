3dd35c36b4a3e9460b9abd7ff57a2e80b01db47b
Joe Brady <joe.brady@uk.ibm.com>
2019-01-11 16:05:43 +0000

    Meta tags fix

--- client/document.js
+++ client/document.js
@@ -1,13 +1,14 @@
 const createFullPageHtml = (appHtml, appBundles, appStyles) =>
   `<html>
     <head>
-      <link rel="icon" href="static/favicon.ico">
-      <title>React SSR and code-splitting template</title>
-      ${appStyles}
+      <meta charset="utf-8">
       <meta
         name="viewport"
         content="minimum-scale=1,initial-scale=1, width=device-width, shrink-to-fit=no"
       >
+      <link rel="icon" href="static/favicon.ico">
+      <title>React SSR and code-splitting template</title>
+      ${appStyles}
     </head>
     <body>
       <div id="root">${appHtml}</div>
