ca68fadc5a94296a5f5cec0ae1b522f26ed2166a
Joe Brady <joe.brady@uk.ibm.com>
2019-01-11 14:02:42 +0000

    Add jest setup comments

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,10 @@
 /* eslint-disable */
 import React from "react";
 
+// Provide React globally during tests. Necessary to match global React provided in webpack config.
 global.React = React;
 
+// Make tests fail when Jest logs a failed prop type error.
 const originalConsoleError = console.error;
 console.error = message => {
   if (/(Failed prop type)/.test(message)) {
